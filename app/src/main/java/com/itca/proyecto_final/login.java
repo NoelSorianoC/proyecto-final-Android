ad99ab5f62e7a2d9557372079b392990a659d750
NoelSorianoC <82602830+NoelSorianoC@users.noreply.github.com>
2021-11-04 11:58:01 -0600

    reparacion de codigo
    login

---
+++ app/src/main/java/com/itca/proyecto_final/login.java
@@ -0,0 +1,14 @@
+package com.itca.proyecto_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
